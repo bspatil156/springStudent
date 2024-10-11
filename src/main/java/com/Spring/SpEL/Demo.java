65135c1b08a1942b923c2b66aa06fc9a6517be4e
bspatil156 <mr.bspatil156@gmail.com>
2024-10-11 17:23:43 +0530

    Done creating objects using SpEL and calling methods and variables.

--- src/main/java/com/Spring/SpEL/Demo.java
+++ src/main/java/com/Spring/SpEL/Demo.java
@@ -10,6 +10,16 @@ public class Demo {
     private int x;
     @Value("#{20 * 20}")
     private int y;
+    @Value("#{T(java.lang.Math).sqrt(4)}")
+    private int z;
+    @Value("#{T(java.lang.Math).E}")
+    private double epsilon;
+    @Value("#{new java.lang.String('Stay Hungry Stay Foolish')}")
+    private String message;
+    @Value("#{12>2}")
+    private boolean condition;
+//    @Value("#{new com.Spring.SpEL.Demo()}")
+//    private Demo obj;
 
     public int getX() {
         return x;
@@ -27,11 +37,55 @@ public class Demo {
         this.y = y;
     }
 
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public double getEpsilon() {
+        return epsilon;
+    }
+
+    public void setEpsilon(double epsilon) {
+        this.epsilon = epsilon;
+    }
+
+//    public Demo getObj() {
+//        return obj;
+//    }
+//
+//    public void setObj(Demo obj) {
+//        this.obj = obj;
+//    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isCondition() {
+        return condition;
+    }
+
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
+                ", epsilon=" + epsilon +
+                ", message='" + message + '\'' +
+                ", condition=" + condition +
                 '}';
     }
 }
